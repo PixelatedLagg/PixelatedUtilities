2a738ec1d434a9588fc7d4fe44d42f14cf5ab0d3
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2021-08-19 20:10:27 -0400

    Initial Commit

---
+++ PixelatedUtilities/TypeManipulation/Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelatedUtilities
+{
+    public static class Number
+    {
+        /// <summary>
+        /// Generates a random number within the lowest value and highest value.
+        /// Note: The random number can include both the lowest value AND the highest value.
+        /// </summary>
+        /// <param name="lowestValue"></param>
+        /// <param name="highestValue"></param>
+        /// <returns></returns>
+        public static int RandomInt(int lowestValue, int highestValue)
+        {
+            highestValue++;
+            Random r = new Random();
+            return r.Next(lowestValue, highestValue);
+        }
+        /// <summary>
+        /// Returns "even" if value is even and "odd" if value is odd.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string EvenOrOdd(int value){if(value%2==0){return "even";}else{return "odd";}}
+    }
+}
