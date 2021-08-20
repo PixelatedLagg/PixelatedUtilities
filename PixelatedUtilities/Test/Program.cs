d4eecc86eb0a6d8ebfe75454e0149fcfa1cddc6a
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2021-08-19 23:03:34 -0400

    organization commit

--- PixelatedUtilities/Test/Program.cs
+++ PixelatedUtilities/Test/Program.cs
@@ -8,24 +8,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Testing PixelatedUtilities...");
-            //Dictionary:
-            //Random KeyValuePair
-            //Get key by value
-            //
-            //Numbers:
-            Dictionary<string, int> dict = new();
-            dict.Add("a", 1);
-            dict.Add("b", 2);
-            dict.Add("c", 3);
-
-            Pack<int, int> pack = new(); //Stores 2 unique values (inspired by KeyValuePair)
-            pack.SetValues(1, 2); //Sets both values
-
-            Console.WriteLine(pack.Value1 + pack.Value2);
-            Console.WriteLine(pack.GetType()); //Finds type
-            Console.WriteLine(dict.KeyHasValue(2)); //Finds key by value
-            Console.WriteLine(dict.RandomPair()); //Finds random pair
+            //used for testing code
         }
     }
 }
