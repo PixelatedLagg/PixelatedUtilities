cd9b2fe4ae144dda561bc681c430027d04568cce
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-01-08 18:00:16 -0500

    overhaul to srandom

--- src/PUtils/Misc/SRandom.cs
+++ src/PUtils/Misc/SRandom.cs
@@ -1,11 +1,12 @@
 using System;
-using System.Collections.Generic;
+using System.Text;
 
 namespace PUtils
 {
     public class SRandom
     {
-        private const long m = 4294967296; // aka 2^32
+        //SRandom() and Long() credited to: ekepes (github)
+        private const long m = 4294967296;
         private const long a = 1664525;
         private const long c = 1013904223;
         private long _last;
@@ -18,38 +19,45 @@ namespace PUtils
             _last = ((a * _last) + c) % m;
             return _last;
         }
+        public long Range(long min, long max)
+        {
+            return Long() % (max - min) + min;
+        }
         public int Single()
         {
-            string result = Long().ToString();
-            return result.ToCharArray()[result.Length - 1];
+            return (int)Range(0, 9);
+        }
+        public double Double()
+        {
+            return Convert.ToDouble($"0.{Range(0, 99)}");
+        }
+        public int Int()
+        {
+            return (int)Range(-2147483648, 2147483647);
+        }
+        public ushort UShort()
+        {
+            return (ushort)Range(0, 65535);
+        }
+        public long Length(int length)
+        {
+            if (length <= 0)
+            {
+                return 0;
+            }
+            StringBuilder top = new StringBuilder(length);
+            StringBuilder bottom = new StringBuilder(length);
+            bottom.Append('1');
+            for (int x = 0; x < length; x++)
+            {
+                top.Append('9');
+                if (bottom.Length == length)
+                {
+                    continue;
+                }
+                bottom.Append('0');
+            }
+            return Range(Convert.ToInt64(bottom.ToString()), Convert.ToInt64(top.ToString()));
         }
     }
 }
-/*
-private const long m = 4294967296; // aka 2^32
-private const long a = 1664525;
-private const long c = 1013904223;
-private long _last;
-
-public RandomNumberGenerator()
-{
-    _last = DateTime.Now.Ticks % m;
-}
-
-public RandomNumberGenerator(long seed)
-{
-    _last = seed;
-}
-
-public long Next()
-{
-    _last = ((a * _last) + c) % m;
-
-    return _last;
-}
-
-public long Next(long maxValue)
-{
-    return Next() % maxValue;
-}
-*/
